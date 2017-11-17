7989e298f27ebdc3d44c0bc4a87a3e30aa275843
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2017-11-17 21:47:50 +0200

    Params for simulate.

--- simulate.py
+++ simulate.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 
 from index import get_rebalance_orders, get_portfolio_value
@@ -16,14 +17,27 @@ def portfolio_to_balance(portfolio):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Simulate BTC indexing across historical data.")
+    parser.add_argument('--top-limit', dest='top_limit',
+                        default=25, type=int,
+                        help="Number of best coins to choose when indexing")
+    parser.add_argument('--weeks-back', dest='weeks_back',
+                        default=52, type=int,
+                        help="Number of weeks back to simulate")
+    parser.add_argument('--rebalance-period', dest='rebalance_period',
+                        default=1, type=int,
+                        help="How often (in weeks) to rebalance")
+
+    args = parser.parse_args()
+
     with open('scrape_results.json') as f:
         historical_data = json.load(f)
 
     # Sort by week asc.
     historical_data.sort(key=lambda x: x[0])
 
-    for week, market_data in historical_data[-8:]:
-        orders, portfolio = get_rebalance_orders(market_data, balance, top_limit=300)
+    for week, market_data in historical_data[-args.weeks_back::args.rebalance_period]:
+        orders, portfolio = get_rebalance_orders(market_data, balance, top_limit=args.top_limit)
         balance = portfolio_to_balance(portfolio)
 
     prices = {item['name']: item['price'] for item in market_data}
