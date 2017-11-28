a83bbb02eb0ebaefa5f1792767634c5f488b8eac
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2017-11-28 19:59:25 +0200

    Robust coinmarketcap.

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+from constants import MAIN_CURRENCY
+
 
 def get_coinmarketcap():
     r = requests.get('https://coinmarketcap.com/all/views/all/')
@@ -19,12 +21,21 @@ def scrape_coins(soup):
             # Sometimes value is '?' for market cap.
             market_cap = 0
 
-        coin_data = {
-            'price': float(row.find('a', {'class': 'price'}).get('data-btc')),
-            'name': row.find_all('td')[2].text,
-            'volume': float(row.find('a', {'class': 'volume'}).get('data-btc')),
-            'market_cap': market_cap
-        }
+        try:
+            coin_data = {
+                'price': float(row.find('a', {'class': 'price'}).get('data-btc')),
+                'name': row.find_all('td')[2].text,
+                'volume': float(row.find('a', {'class': 'volume'}).get('data-btc')),
+                'market_cap': market_cap
+            }
+        except ValueError:
+            # Some coins have missing fields.
+            continue
+
+        # Skip main currency, can't "buy" it.
+        if coin_data['name'] == MAIN_CURRENCY:
+            continue
+
         data.append(coin_data)
 
     return data
