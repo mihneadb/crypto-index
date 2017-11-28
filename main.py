a965e42fe373577960ef352020d94d0dfbdfa101
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2017-11-28 20:18:06 +0200

    Just do it.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from coinmarketcap import get_coinmarketcap
 from constants import MAIN_CURRENCY, ValueKeys, Actions
 from index import get_rebalance_orders
-from market_bittrex import get_balance, get_market_data
+from market_bittrex import get_balance, get_market_data, exec_order_spec
 
 
 if __name__ == '__main__':
@@ -9,14 +10,20 @@ if __name__ == '__main__':
 
     balance = get_balance()
     market_data = get_market_data(MAIN_CURRENCY)
+    global_market_data = get_coinmarketcap(MAIN_CURRENCY)
 
-    orders, ideal_portfolio = get_rebalance_orders(market_data, balance, top_limit=TOP_LIMIT, value_key=VALUE_KEY)
+    orders, ideal_portfolio = get_rebalance_orders(market_data, global_market_data,
+                                                   balance,
+                                                   top_limit=TOP_LIMIT, value_key=VALUE_KEY)
     sell_orders = [o for o in orders if o.action == Actions.SELL]
     buy_orders = [o for o in orders if o.action == Actions.BUY]
 
     from pprint import pprint
     print "Balance now:"
     pprint(balance)
+
+    print "Ideal portfolio:"
+    pprint(ideal_portfolio)
 
     print "To sell:"
     pprint(sell_orders)
@@ -25,7 +32,7 @@ if __name__ == '__main__':
     pprint(buy_orders)
 
     # for order in sell_orders:
-    #     exec_order_spec(order)
+    #     pprint(exec_order_spec(order))
     #
     # for order in buy_orders:
-    #     exec_order_spec(order)
+    #     pprint(exec_order_spec(order))
