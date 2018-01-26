f0fb4e35ca14d0d23ef79ea7cdc9e7aed65107d8
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2018-01-26 17:03:59 +0200

    Use ccxt for trading on binance.

--- market_binance.py
+++ market_binance.py
@@ -1,14 +1,22 @@
 import json
 
 from binance.client import Client
+import ccxt
 
 from constants import MAIN_CURRENCY, Actions
+from helpers import truncate
 
 
 def get_binance():
     with open('binance_key.json') as f:
         data = json.load(f)
         return Client(data['key'], data['secret'])
+
+
+def get_binance_ccxt():
+    with open('binance_key.json') as f:
+        data = json.load(f)
+        return ccxt.binance({'apiKey': data['key'], 'secret': data['secret']})
 
 
 def get_balance():
@@ -25,14 +33,25 @@ def get_balance_entry(balance):
 
 
 def exec_order_spec(order_spec):
-    b = get_binance()
-    symbol = '%s%s' % (order_spec.coin, MAIN_CURRENCY)
+    b = get_binance_ccxt()
 
+    symbol = '%s/%s' % (order_spec.coin, MAIN_CURRENCY)
     if order_spec.action == Actions.BUY:
-        fn = b.order_limit_buy
+        fn = b.create_limit_buy_order
     elif order_spec.action == Actions.SELL:
-        fn = b.order_limit_sell
+        fn = b.create_limit_sell_order
     else:
         raise ValueError("Invalid action")
 
-    return fn(symbol=symbol, quantity=order_spec.amount, price=str(order_spec.price))
+    return fn(symbol, order_spec.amount, order_spec.price)
+
+    # symbol = '%s%s' % (order_spec.coin, MAIN_CURRENCY)
+    #
+    # if order_spec.action == Actions.BUY:
+    #     fn = b.order_limit_buy
+    # elif order_spec.action == Actions.SELL:
+    #     fn = b.order_limit_sell
+    # else:
+    #     raise ValueError("Invalid action")
+    #
+    # return fn(symbol=symbol, quantity=truncate(order_spec.amount, 3), price='%f' % order_spec.price)
